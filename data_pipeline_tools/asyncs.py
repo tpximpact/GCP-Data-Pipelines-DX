df1e51211e878b789f457f892cc1597094c5703b
urquha <andy.urquhart@hotmail.co.uk>
2023-02-20 23:57:32 +0000

    Added poetry

--- data_pipeline_tools/asyncs.py
+++ data_pipeline_tools/asyncs.py
@@ -4,14 +4,22 @@ import aiohttp
 import pandas as pd
 
 
-async def get_data(url, headers):
+async def get_data(url: str, headers: dict) -> dict:
+    # This is an asynchronous function that uses aiohttp to make an HTTP GET request to a URL with the given headers.
+    # It returns the response data as a JSON object.
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=headers) as response:
             data = await response.json()
             return data
 
 
-async def get_data_for_page_range(url, start_page, end_page, headers, key):
+async def get_data_for_page_range(
+    url: str, start_page: int, end_page: int, headers: dict, key: str
+) -> pd.DataFrame:
+    # This is an asynchronous function that gets data from a range of pages using the get_data() function.
+    # It creates a list of tasks for the given page range and uses asyncio.gather() to execute them in parallel.
+    # It uses the key to get the required values from the response data and returns it as a list of dictionaries.
+    # It uses the list of dictionaries to create a Pandas DataFrame.
     tasks = []
     # async with aiohttp.ClientSession() as session:
     for i in range(start_page, end_page + 1):
@@ -25,7 +33,12 @@ async def get_data_for_page_range(url, start_page, end_page, headers, key):
     return df
 
 
-async def get_all_data(url, headers, pages, key, batch_size=10):
+async def get_all_data(
+    url: str, headers: dict, pages: int, key: str, batch_size: int = 10
+) -> pd.DataFrame:
+    # This is an asynchronous function that gets data from all pages in batches using the get_data_for_page_range() function.
+    # It divides the page range into batches of batch_size pages and gets data for each batch in parallel using asyncio.gather().
+    # It concatenates the data from each batch into a single Pandas DataFrame and returns it.
     dfs = []
     for start_page in range(1, pages + 1, batch_size):
         end_page = min(start_page + batch_size - 1, pages)
--- data_pipeline_tools/util.py
+++ data_pipeline_tools/util.py
@@ -1,17 +1,29 @@
 import pandas as pd
+from google.api_core.exceptions import BadRequest
 from google.cloud import bigquery, secretmanager
 
 
-def write_to_bigquery(config: dict, df: pd.DataFrame, write_disposition: str):
+def write_to_bigquery(config: dict, df: pd.DataFrame, write_disposition: str) -> None:
+    # Create a BigQuery client with the specified location.
     client = bigquery.Client(location=config["location"])
 
+    # Get a reference to the BigQuery table to write to.
     dataset_ref = client.dataset(config["dataset_id"])
     table_ref = dataset_ref.table(config["table_name"])
+
+    # Set up the job configuration with the specified write disposition.
     job_config = bigquery.LoadJobConfig(write_disposition=write_disposition)
     job_config.autodetect = True
 
-    job = client.load_table_from_dataframe(df, table_ref, job_config=job_config)
-    job.result()
+    try:
+        # Write the DataFrame to BigQuery using the specified configuration.
+        job = client.load_table_from_dataframe(df, table_ref, job_config=job_config)
+        job.result()
+    except BadRequest as e:
+        print(f"Error writing DataFrame to BigQuery: {str(e)}")
+        return
+
+    # Print a message indicating how many rows were loaded.
     print(
         "Loaded {} rows into {}:{}.".format(
             job.output_rows, config["dataset_id"], config["table_name"]
@@ -19,36 +31,33 @@ def write_to_bigquery(config: dict, df: pd.DataFrame, write_disposition: str):
     )
 
 
-def flatten_columns(df):
-    # Flatten nested JSON values using Pandas' json_normalize function
+def flatten_columns(df: pd.DataFrame) -> pd.DataFrame:
+    # Find all columns that have a dictionary as a value in the first row of the DataFrame.
+
     nested_columns = [
         column_name
         for column_name, value in df.iloc[0].items()
         if isinstance(value, dict)
     ]
 
+    # For each nested column, flatten the JSON values using Pandas' json_normalize function.
     for column in nested_columns:
         flattened_df = pd.json_normalize(df[column], max_level=1).add_prefix(
             f"{column}_"
         )
 
-        flattened_df = flattened_df
-
+        # Add the flattened columns to the DataFrame and drop the original nested column.
         df = pd.concat([df, flattened_df], axis=1)
         df = df.drop(column, axis=1)
 
     return df
 
 
-def access_secret_version(project_id, secret_id, version_id="latest"):
-    # Create the Secret Manager client.
+def access_secret_version(
+    project_id: str, secret_id: str, version_id: str = "latest"
+) -> str:
+    # Create the Secret Manager client and get the secret payload.
     client = secretmanager.SecretManagerServiceClient()
-
-    # Build the resource name of the secret version.
     name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
-
-    # Access the secret version.
     response = client.access_secret_version(name=name)
-
-    # Return the decoded payload.
     return response.payload.data.decode("UTF-8")
