a870f5d62df4e02a92f9bd6482edbe2b26037ce3
urquha <andy.urquhart@hotmail.co.uk>
2023-02-21 00:26:21 +0000

    setup.py

--- data-pipeline-tools/setup.py
+++ data-pipeline-tools/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="data-pipeline-tools",
     version="0.1",
-    packages=find_packages(),
+    packages=['data_pipeline_tools'],
     url="https://github.com/tpximpact/GCP-Data-Pipelines.git",
     install_requires=["pandas", "aiohttp", "google-cloud"],
 )
