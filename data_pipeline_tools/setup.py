4598e9313a12d7a1ff0fe479f912af59f9378fe5
urquha <andy.urquhart@hotmail.co.uk>
2023-02-21 00:13:18 +0000

    Renamed folder

--- data_pipeline_tools/setup.py
+++ data_pipeline_tools/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="data_pipeline_tools",
+    name="data-pipeline-tools",
     version="0.1",
     packages=find_packages(),
     install_requires=["pandas", "aiohttp", "google-cloud"],
