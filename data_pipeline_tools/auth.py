dad3ca263d402daa7e925063f2de2d55a36239b8
urquha <andy.urquhart@hotmail.co.uk>
2023-02-21 19:01:13 +0000

    Added Pipedrive access

--- data_pipeline_tools/auth.py
+++ data_pipeline_tools/auth.py
@@ -3,9 +3,13 @@ from .util import access_secret_version
 
 def harvest_headers(project_id, service):
     return {
-        "User-Agent": "Harvest Data Visualisation",
+        "User-Agent": "TPX Cloud Functions",
         "Authorization": "Bearer "
         + access_secret_version(project_id, "HARVEST_ACCESS_TOKEN"),
         "Harvest-Account-ID": access_secret_version(project_id, "HARVEST_ACCOUNT_ID"),
         "service": service,
     }
+
+def pipedrive_access_token(project_id):
+    return access_secret_version(project_id, "PIPEDRIVE_ACCESS_TOKEN")
+    
