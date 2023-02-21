06420f24f1a8947ec354c2bd45b149907bd961e5
urquha <andy.urquhart@hotmail.co.uk>
2023-02-21 00:39:18 +0000

    Another restructure

--- data_pipeline_tools/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="data-pipeline-tools.data_pipeline_tools",
-    version="0.1",
-    packages=['data_pipeline_tools'],
-    url="https://github.com/tpximpact/GCP-Data-Pipelines.git",
-    install_requires=["pandas", "aiohttp", "google-cloud"],
-)
