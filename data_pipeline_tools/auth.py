f20d1be1ea2e55d7292486d19011411cc2c5785d
urquha <andy.urquhart@hotmail.co.uk>
2023-02-23 17:21:45 +0000

    Added content type Header

--- data_pipeline_tools/auth.py
+++ data_pipeline_tools/auth.py
@@ -8,6 +8,7 @@ def harvest_headers(project_id, service):
         + access_secret_version(project_id, "HARVEST_ACCESS_TOKEN"),
         "Harvest-Account-ID": access_secret_version(project_id, "HARVEST_ACCOUNT_ID"),
         "service": service,
+        'Content-Type': 'application/json'
     }
 
 def pipedrive_access_token(project_id):
