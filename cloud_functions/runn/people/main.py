a5d7646ee9664bd632eda49b056249974a6f758b
Richard Hanson <richard.hanson@manifesto.co.uk>
2024-10-12 00:42:30 +0100

    Refactored Runn People function

--- cloud_functions/runn/people/main.py
+++ cloud_functions/runn/people/main.py
@@ -3,66 +3,74 @@ import pandas as pd
 import requests
 
 from data_pipeline_tools.auth import runn_headers
-from data_pipeline_tools.util import (
-  handle_runn_rate_limits,
+
+from data_pipeline_tools.bigquery_helpers import (
+  bigquery_client_get,
   write_to_bigquery
+)
+
+from data_pipeline_tools.runn_tools import (
+  reference_value_get
 )
 
 project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
 
 if not project_id:
-    project_id = "tpx-dx-dashboards"
+  project_id = "tpx-dx-dashboards"
 
 
 def load_config(project_id, service) -> dict:
-    return {
-        "url": "https://api.runn.io/people",
-        "headers": runn_headers(project_id, service),
-        "dataset_id": os.environ.get("DATASET_ID"),
-        "gcp_project": project_id,
-        "table_name": os.environ.get("TABLE_NAME"),
-        "location": os.environ.get("TABLE_LOCATION"),
-        "service": service,
-    }
-
-def get_harvest_id(references):
-    if references:
-        if references[0]["referenceName"] == "Harvest":
-            return str(references[0]["externalId"])
-
-    return ""
+  return {
+    "url"        : "https://api.runn.io/people",
+    "headers"    : runn_headers(project_id, service),
+    "dataset_id" : os.environ.get("DATASET_ID") if os.environ.get("DATASET_ID") else "Runn_Raw",
+    "gcp_project": project_id,
+    "table_name" : os.environ.get("TABLE_NAME") if os.environ.get("TABLE_NAME") else "people",
+    "location"   : os.environ.get("TABLE_LOCATION") if os.environ.get("TABLE_LOCATION") else "europe-west2",
+    "service"    : service
+  }
 
 def main(data: dict, context):
-    service = "Data Pipeline - Runn People"
-    config = load_config(project_id, service)
-    people = []
-    next_cursor = ""
+  service = "Data Pipeline - Runn People"
+  config = load_config(project_id, service)
+  people = []
+  next_cursor = ""
 
-    while True:
-        url = config["url"] + "?cursor=" + next_cursor if next_cursor else config["url"]
-        response = requests.get(url=url, headers=config["headers"])
+  while True:
+    url = config["url"] + "?cursor=" + next_cursor if next_cursor else config["url"]
+    print("getting page", url)
 
-        if response.status_code == 200:
-            data = response.json()
+    response = requests.get(url=url, headers=config["headers"])
 
-            people.extend(data.get("values", []))
-            next_cursor = data.get("nextCursor")
+    if response.status_code == 200:
+      data = response.json()
 
-            if not next_cursor:
-                break
-        else:
-            raise Exception(f"Failed to fetch people: {response.status_code}, {response.text}")
+      people.extend(data.get("values", []))
+      next_cursor = data.get("nextCursor")
 
-        people_df = pd.DataFrame(people)
+      if not next_cursor:
+        break
+    else:
+      raise Exception(f"Failed to fetch people: {response.status_code}, {response.text}")
 
-        harvest_ids = people_df["references"].apply(get_harvest_id)
-        people_df["harvest_id"] = harvest_ids
-        people_df = people_df.drop(columns=["references", "tags"])
+  df = pd.DataFrame(people)
 
-        write_to_bigquery(config, people_df, "WRITE_TRUNCATE")
-        handle_runn_rate_limits(response)
+  df["harvestId"] = df["references"].apply(lambda references: reference_value_get("Harvest", references))
+  df["createdAt"] = df["createdAt"].apply(lambda dateString: pd.Timestamp(dateString))
+  df["updatedAt"] = df["updatedAt"].apply(lambda dateString: pd.Timestamp(dateString))
+  df = df.drop(columns=["references", "tags"])
 
-        print(f"Total number of people fetched: {len(people_df)}")
+  bigquery_client = bigquery_client_get(location=config["location"])
+
+  write_to_bigquery(
+    client=bigquery_client,
+    dataset_id=config["dataset_id"],
+    table_name=config["table_name"],
+    df=df,
+    write_disposition="WRITE_TRUNCATE"
+  )
+
+  print(f"Total number of people fetched: {len(df)}")
 
 
 if __name__ == "__main__":
