f835029e47055043ecac820d2b062984abb320ba
Benjamin Dudiak-Fry <ben.dudiak-fry@manifesto.co.uk>
2025-10-01 17:51:29 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="data-pipeline-tools",
-    version="1.0",
+    version="1.0.1",
     packages=["data_pipeline_tools"],
     url="https://github.com/tpximpact/GCP-Data-Pipelines.git",
     install_requires=["pandas", "aiohttp", "google-cloud"],
