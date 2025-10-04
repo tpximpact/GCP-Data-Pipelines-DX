3c5a083c51c8a120d9e83107e7e1616e607c1984
Benjamin Dudiak-Fry <ben.dudiak-fry@manifesto.co.uk>
2025-10-04 18:40:07 +0100

    Add tools to fetch data from runn apis to data_pipeline_tools

--- data_pipeline_tools/runn_tools.py
+++ data_pipeline_tools/runn_tools.py
@@ -1,4 +1,10 @@
 import time
+import pandas as pd
+from data_pipeline_tools.auth import runn_headers_base
+import requests
+
+DEFAULT_MAX_ATTEMPTS = 5
+DEFAULT_PAGE_SIZE = 500
 
 
 def reference_value_get(reference_name: str, references: list):
@@ -20,3 +26,69 @@ def handle_runn_rate_limits(response):
         wait_time = max(rate_limit_reset, retry_after)
         print(f"Rate limit reached. Waiting for {wait_time} seconds.")
         time.sleep(wait_time)
+
+
+def page_get(
+    url,
+    headers,
+    page_size,
+    attempt=0,
+    max_attempts=DEFAULT_MAX_ATTEMPTS,
+):
+    response = requests.get(url=url, headers=headers)
+    if response.status_code == 200:
+        print("OK")
+        data = response.json()
+        next_cursor = data.get("nextCursor")
+
+        df = pd.DataFrame(data.get("values", []))
+
+        if df.empty:
+            return df, ""
+
+        handle_runn_rate_limits(response)
+
+        return df, next_cursor
+    elif response.status_code == 429:
+        print(f"Rate limit exceeded, attempt {attempt}")
+        if attempt > max_attempts:
+            raise Exception(
+                f"Max attempts {max_attempts} exceeded: {response.status_code}, {response.text}"
+            )
+        handle_runn_rate_limits(response)
+        return page_get(url, headers, page_size, attempt=attempt + 1)
+
+    else:
+        if attempt > max_attempts:
+            raise Exception(
+                f"Max attempts {max_attempts} exceeded: {response.status_code}, {response.text}"
+            )
+
+        print(
+            f"(Attempt {attempt}) Status code {response.status_code} returned. Retrying.."
+        )
+        return page_get(url, headers, page_size, attempt=attempt + 1)
+
+
+def fetch_all(token, base_url, service, page_size=DEFAULT_PAGE_SIZE):
+    next_cursor = None
+    has_more = True
+    page_no = 0
+
+    headers = runn_headers_base(token, service)
+
+    while has_more:
+        url = (
+            f"{base_url}?cursor={next_cursor}&limit={page_size}"
+            if next_cursor
+            else f"{base_url}?limit={page_size}"
+        )
+
+        print(f"Page {page_no}, fetching {url}")
+        page_df, next_cursor = page_get(url, headers, page_size)
+        yield page_df
+        page_no = page_no + 1
+
+        if not next_cursor:
+            has_more = False
+            break
