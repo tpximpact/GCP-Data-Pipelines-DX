ac09930bdd4d111a6c84c8d141b9b222b3843165
Benjamin Dudiak-Fry <ben.dudiak-fry@manifesto.co.uk>
2025-10-05 21:14:05 +0100

    Refactor runn 'projects' function to use standard functions

--- cloud_functions/runn/projects/main.py
+++ cloud_functions/runn/projects/main.py
@@ -1,35 +1,28 @@
 import os
 import pandas as pd
-import requests
 
-from data_pipeline_tools.auth import runn_headers
+from datetime import datetime, timezone
+from data_pipeline_tools.auth import runn_headers, access_secret_version
 
 from data_pipeline_tools.bigquery_helpers import bigquery_client_get, write_to_bigquery
+from data_pipeline_tools.util import target_daily_partition
 
-from data_pipeline_tools.runn_tools import reference_value_get, handle_runn_rate_limits
+from data_pipeline_tools.runn_tools import reference_value_get
+from data_pipeline_tools.runn_tools import fetch_all
+from itertools import batched
 
-project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+BATCH_SIZE = 50
 
-if not project_id:
-    project_id = "tpx-dx-dashboards"
+project_id = os.environ.get("GOOGLE_CLOUD_PROJECT") or "tpx-dx-dashboards"
 
 
 def load_config(project_id, service) -> dict:
     return {
-        "url": "https://api.runn.io/projects",
         "headers": runn_headers(project_id, service),
-        "dataset_id": (
-            os.environ.get("DATASET_ID") if os.environ.get("DATASET_ID") else "Runn_Raw"
-        ),
+        "dataset_id": (os.environ.get("DATASET_ID") or "Runn_Raw"),
         "gcp_project": project_id,
-        "table_name": (
-            os.environ.get("TABLE_NAME") if os.environ.get("TABLE_NAME") else "projects"
-        ),
-        "location": (
-            os.environ.get("TABLE_LOCATION")
-            if os.environ.get("TABLE_LOCATION")
-            else "europe-west2"
-        ),
+        "table_name": os.environ.get("TABLE_NAME") or "projects",
+        "location": (os.environ.get("TABLE_LOCATION") or "europe-west2"),
         "service": service,
     }
 
@@ -52,34 +45,31 @@ def get_first_project_tag(tags):
     return "No tag"
 
 
-def main(data: dict, context):
-    service = "Data Pipeline - Runn Projects"
-    config = load_config(project_id, service)
-    projects = []
-    next_cursor = ""
+def date_pd_timestamp(dateString):
+    return pd.Timestamp(dateString)
 
-    while True:
-        url = config["url"] + "?cursor=" + next_cursor if next_cursor else config["url"]
-        print("getting page", url)
 
-        response = requests.get(url=url, headers=config["headers"])
+def spent_date_pd_timestamp(dateString):
+    return pd.Timestamp(f"{dateString}T00:00:00Z")
 
-        if response.status_code == 200:
-            data = response.json()
-            projects.extend(data.get("values", []))
-            next_cursor = data.get("nextCursor")
 
-            if not next_cursor:
-                break
-            else:
-                handle_runn_rate_limits(response)
+def process_dataframe(df):
 
-        else:
-            raise Exception(
-                f"Failed to fetch people: {response.status_code}, {response.text}"
-            )
+    df["id"] = df["id"].astype("Int64")
+    df["name"] = df["name"].astype(str)
+    df["isTemplate"] = df["isTemplate"].astype(bool)
+    df["isArchived"] = df["isArchived"].astype(bool)
+    df["isConfirmed"] = df["isConfirmed"].astype(bool)
+    df["pricingModel"] = df["pricingModel"].astype(str)
+    df["rateType"] = df["rateType"].astype(str)
+    df["teamId"] = df["teamId"].astype("Int64")
+    df["budget"] = pd.to_numeric(df["budget"], errors="coerce")
+    df["expensesBudget"] = pd.to_numeric(df["expensesBudget"], errors="coerce")
+    df["clientId"] = df["clientId"].astype("Int64")
+    df["rateCardId"] = df["rateCardId"].astype("Int64")
+    df["createdAt"] = df["createdAt"].apply(lambda dateString: pd.Timestamp(dateString))
+    df["updatedAt"] = df["updatedAt"].apply(lambda dateString: pd.Timestamp(dateString))
 
-    df = pd.DataFrame(projects)
     df["harvestId"] = df["references"].apply(
         lambda references: reference_value_get("Harvest", references)
     )
@@ -92,17 +82,40 @@ def main(data: dict, context):
     df["projectType"] = df["tags"].apply(get_first_project_tag)
     df = df.drop(columns=["references", "customFields", "tags"])
 
+    return df
+
+
+def main(data: dict, context):
+    service = "Data Pipeline - Projects"
+    now = datetime.now(timezone.utc)
+    config = load_config(project_id, service)
+
+    runn_api_token = access_secret_version(project_id, "RUNN_ACCESS_TOKEN")
     bigquery_client = bigquery_client_get(location=config["location"])
 
-    write_to_bigquery(
-        client=bigquery_client,
-        dataset_id=config["dataset_id"],
-        table_name=config["table_name"],
-        df=df,
-        write_disposition="WRITE_TRUNCATE",
+    pages = fetch_all(
+        token=runn_api_token,
+        base_url="https://api.runn.io/projects/",
+        service=service,
+        # Max page_size for /projects is 200
+        page_size=200,
     )
 
-    print(f"Total number of projects fetched: {len(projects)}")
+    for batch_num, batch in enumerate(batched(pages, BATCH_SIZE)):
+        # First frame truncates partition
+        # Subsequent frames append
+        disposition = "WRITE_TRUNCATE_DATA" if batch_num == 0 else "WRITE_APPEND"
+
+        df = pd.concat(batch)
+        processed_df = process_dataframe(df)
+
+        write_to_bigquery(
+            client=bigquery_client,
+            dataset_id=config["dataset_id"],
+            table_name=target_daily_partition(config["table_name"], now),
+            df=processed_df,
+            write_disposition=disposition,
+        )
 
 
 if __name__ == "__main__":
