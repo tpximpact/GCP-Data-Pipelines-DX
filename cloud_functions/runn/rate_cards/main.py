a466d211048f6a2eeceac78d09e4101d47255cbb
Benjamin Dudiak-Fry <ben.dudiak-fry@manifesto.co.uk>
2025-10-05 21:40:40 +0100

    Refactor 'rate-cards' cloud function to use standard functions and correctly use partitions

--- cloud_functions/runn/rate_cards/main.py
+++ cloud_functions/runn/rate_cards/main.py
@@ -1,121 +1,101 @@
 import os
 import pandas as pd
-import requests
 
-from data_pipeline_tools.auth import runn_headers
+from datetime import datetime, timezone
+from data_pipeline_tools.auth import runn_headers, access_secret_version
 
 from data_pipeline_tools.bigquery_helpers import bigquery_client_get, write_to_bigquery
+from data_pipeline_tools.util import target_daily_partition
 
-from data_pipeline_tools.runn_tools import handle_runn_rate_limits
+from data_pipeline_tools.runn_tools import reference_value_get
+from data_pipeline_tools.runn_tools import fetch_all
+from itertools import batched
 
-project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+BATCH_SIZE = 50
 
-if not project_id:
-    project_id = "tpx-dx-dashboards"
+project_id = os.environ.get("GOOGLE_CLOUD_PROJECT") or "tpx-dx-dashboards"
 
 
 def load_config(project_id, service) -> dict:
     return {
-        "url": "https://api.runn.io/rate-cards",
         "headers": runn_headers(project_id, service),
-        "dataset_id": (
-            os.environ.get("DATASET_ID") if os.environ.get("DATASET_ID") else "Runn_Raw"
-        ),
+        "dataset_id": (os.environ.get("DATASET_ID") or "Runn_Raw"),
         "gcp_project": project_id,
-        "rates_table_name": (
-            os.environ.get("RATES_TABLE_NAME")
-            if os.environ.get("RATES_TABLE_NAME")
-            else "rate_cards"
-        ),
-        "projects_table_name": (
-            os.environ.get("PROJECTS_TABLE_NAME")
-            if os.environ.get("PROJECTS_TABLE_NAME")
-            else "rate_project_rates"
-        ),
-        "location": (
-            os.environ.get("TABLE_LOCATION")
-            if os.environ.get("TABLE_LOCATION")
-            else "europe-west2"
-        ),
+        "table_name": os.environ.get("TABLE_NAME") or "rate_cards",
+        "location": (os.environ.get("TABLE_LOCATION") or "europe-west2"),
         "service": service,
     }
 
 
-def process_results_page(results, df_rates, df_projects):
-    projects = []
+def date_pd_timestamp(dateString):
+    return pd.Timestamp(dateString)
 
-    df = pd.DataFrame(results)
 
-    for index, row in df.iterrows():
-        if row["projectIds"]:
-            for project in row["projectIds"]:
-                projects.append({"projectId": project, "rateCard": row["id"]})
+def spent_date_pd_timestamp(dateString):
+    return pd.Timestamp(f"{dateString}T00:00:00Z")
 
-    df_rates = pd.concat([df_rates, df])
-    df_projects = pd.concat([df_projects, pd.DataFrame(projects)])
 
-    return df_rates, df_projects
+def process_dataframe(df):
+    df["id"] = df["id"].astype("Int64")
+    df["name"] = df["name"].astype(str)
+    df["description"] = df["description"].astype(str)
+    df["isArchived"] = df["isArchived"].astype(bool)
+    df["isBlendedRateCard"] = df["isBlendedRateCard"].astype(bool)
+    df["blendedRate"] = pd.to_numeric(df["blendedRate"], errors="coerce")
+    df["rateType"] = df["rateType"].astype(str)
+
+    df["createdAt"] = df["createdAt"].apply(lambda dateString: pd.Timestamp(dateString))
+    df["updatedAt"] = df["updatedAt"].apply(lambda dateString: pd.Timestamp(dateString))
+
+    df = df[
+        [
+            "id",
+            "name",
+            "description",
+            "isArchived",
+            "isBlendedRateCard",
+            "blendedRate",
+            "rates",
+            "rateType",
+            "createdAt",
+            "updatedAt",
+        ]
+    ]
+
+    return df
 
 
 def main(data: dict, context):
-    service = "Data Pipeline - Runn rate cards"
+    service = "Data Pipeline - People"
+    now = datetime.now(timezone.utc)
     config = load_config(project_id, service)
-    roles = []
-    next_cursor = ""
 
-    df_rates = pd.DataFrame([])
-    df_projects = pd.DataFrame([])
-
-    while True:
-        url = config["url"] + "?cursor=" + next_cursor if next_cursor else config["url"]
-        print("getting page", url)
-
-        response = requests.get(url=url, headers=config["headers"])
-
-        if response.status_code == 200:
-            data = response.json()
-            df_rates, df_projects = process_results_page(
-                data.get("values", []), df_rates, df_projects
-            )
-            next_cursor = data.get("nextCursor")
-
-            if not next_cursor:
-                break
-            else:
-                handle_runn_rate_limits(response)
-
-        else:
-            raise Exception(
-                f"Failed to fetch roles: {response.status_code}, {response.text}"
-            )
-
-    df_rates["createdAt"] = df_rates["createdAt"].apply(
-        lambda dateString: pd.Timestamp(dateString)
-    )
-    df_rates["updatedAt"] = df_rates["updatedAt"].apply(
-        lambda dateString: pd.Timestamp(dateString)
-    )
-    df_rates = df_rates.drop(columns=["projectIds", "references"])
-
+    runn_api_token = access_secret_version(project_id, "RUNN_ACCESS_TOKEN")
     bigquery_client = bigquery_client_get(location=config["location"])
 
-    write_to_bigquery(
-        client=bigquery_client,
-        dataset_id=config["dataset_id"],
-        table_name=config["rates_table_name"],
-        df=df_rates,
-        write_disposition="WRITE_TRUNCATE",
+    pages = fetch_all(
+        token=runn_api_token,
+        base_url="https://api.runn.io/rate-cards",
+        service=service,
+        # Max page_size for /rate-cards is 200
+        page_size=200,
     )
 
-    write_to_bigquery(
-        client=bigquery_client,
-        dataset_id=config["dataset_id"],
-        table_name=config["projects_table_name"],
-        df=df_projects,
-        write_disposition="WRITE_TRUNCATE",
-    )
+    for batch_num, batch in enumerate(batched(pages, BATCH_SIZE)):
+        # First frame truncates partition
+        # Subsequent frames append
+        disposition = "WRITE_TRUNCATE_DATA" if batch_num == 0 else "WRITE_APPEND"
 
-    print("Done")
+        df = pd.concat(batch)
+        processed_df = process_dataframe(df)
+
+        write_to_bigquery(
+            client=bigquery_client,
+            dataset_id=config["dataset_id"],
+            table_name=target_daily_partition(config["table_name"], now),
+            df=processed_df,
+            write_disposition=disposition,
+        )
 
 
 if __name__ == "__main__":
