cf0209ba344f4f1e77fe337c02d8bd70b938db56
Benjamin Dudiak-Fry <ben.dudiak-fry@manifesto.co.uk>
2025-10-05 20:06:28 +0100

    Refactor runn clients function to use standard functions, and only overwrite current partition

--- cloud_functions/runn/clients/main.py
+++ cloud_functions/runn/clients/main.py
@@ -1,21 +1,24 @@
 import os
 import pandas as pd
-import requests
 
-from data_pipeline_tools.auth import runn_headers
 from data_pipeline_tools.bigquery_helpers import bigquery_client_get, write_to_bigquery
+from data_pipeline_tools.util import target_daily_partition
+from data_pipeline_tools.runn_tools import reference_value_get
 
-from data_pipeline_tools.runn_tools import reference_value_get, handle_runn_rate_limits
+from datetime import datetime, timezone
+from data_pipeline_tools.auth import runn_headers, access_secret_version
 
-project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+from data_pipeline_tools.runn_tools import fetch_all
+from itertools import batched
 
-if not project_id:
-    project_id = "tpx-dx-dashboards"
+
+BATCH_SIZE = 50
+
+project_id = os.environ.get("GOOGLE_CLOUD_PROJECT") or "tpx-dx-dashboards"
 
 
 def load_config(project_id, service) -> dict:
     return {
-        "url": "https://api.runn.io/clients",
         "headers": runn_headers(project_id, service),
         "dataset_id": (
             os.environ.get("DATASET_ID") if os.environ.get("DATASET_ID") else "Runn_Raw"
@@ -33,69 +36,56 @@ def load_config(project_id, service) -> dict:
     }
 
 
-def page_get(url, headers):
-    print("getting page", url)
+def date_pd_timestamp(dateString):
+    return pd.Timestamp(dateString)
 
-    response = requests.get(url=url, headers=headers)
 
-    if response.status_code == 200:
-        data = response.json()
-        next_cursor = data.get("nextCursor")
+def spent_date_pd_timestamp(dateString):
+    return pd.Timestamp(f"{dateString}T00:00:00Z")
 
-        df = pd.DataFrame(data.get("values", []))
 
-        if df.empty:
-            return df, ""
-
-        df["harvestId"] = df["references"].apply(
-            lambda references: reference_value_get("Harvest", references)
-        )
-        df["createdAt"] = df["createdAt"].apply(
-            lambda dateString: pd.Timestamp(dateString)
-        )
-        df["updatedAt"] = df["updatedAt"].apply(
-            lambda dateString: pd.Timestamp(dateString)
-        )
-
-        df = df.drop(columns=["references"])
-
-        handle_runn_rate_limits(response)
-
-        return df, next_cursor
-
-    else:
-        raise Exception(
-            f"Failed to fetch clients: {response.status_code}, {response.text}"
-        )
+def process_dataframe(df):
+    df["harvestId"] = df["references"].apply(
+        lambda references: reference_value_get("Harvest", references)
+    )
+    df["createdAt"] = df["createdAt"].apply(lambda dateString: pd.Timestamp(dateString))
+    df["updatedAt"] = df["updatedAt"].apply(lambda dateString: pd.Timestamp(dateString))
+    df = df.drop(columns=["references"])
+    return df
 
 
 def main(data: dict, context):
+    now = datetime.now(timezone.utc)
     service = "Data Pipeline - Runn clients"
+    now = datetime.now(timezone.utc)
     config = load_config(project_id, service)
 
+    runn_api_token = access_secret_version(project_id, "RUNN_ACCESS_TOKEN")
     bigquery_client = bigquery_client_get(location=config["location"])
 
-    next_cursor = ""
-    has_more = True
-    df = pd.DataFrame([])
-
-    while has_more:
-        url = config["url"] + "?cursor=" + next_cursor if next_cursor else config["url"]
-
-        page_df, next_cursor = page_get(url, headers=config["headers"])
-        df = pd.concat([df, page_df])
-
-        if not next_cursor:
-            has_more = False
-            break
-
-    write_to_bigquery(
-        client=bigquery_client,
-        dataset_id=config["dataset_id"],
-        table_name=config["table_name"],
-        df=df,
-        write_disposition="WRITE_TRUNCATE",
+    pages = fetch_all(
+        token=runn_api_token,
+        base_url="https://api.runn.io/clients/",
+        service=service,
+        # /clients max page size is 200
+        page_size=200,
     )
+
+    for batch_num, batch in enumerate(batched(pages, BATCH_SIZE)):
+        # First frame truncates partition
+        # Subsequent frames append
+        disposition = "WRITE_TRUNCATE_DATA" if batch_num == 0 else "WRITE_APPEND"
+
+        df = pd.concat(batch)
+
+        processed_df = process_dataframe(df)
+        write_to_bigquery(
+            client=bigquery_client,
+            dataset_id=config["dataset_id"],
+            table_name=target_daily_partition(config["table_name"], now),
+            df=processed_df,
+            write_disposition=disposition,
+        )
 
 
 if __name__ == "__main__":
